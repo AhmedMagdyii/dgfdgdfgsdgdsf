499dee6d8a6709aa6828dbf8ad197006e54a8bd4
gmzglmz;glmz;gm <44279195+AhmedMagdyii@users.noreply.github.com>
2018-12-28 00:27:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,46 +17,20 @@ const prefix = '*'
  
 
 
-const adminprefix = "*";
-const devs = ['502761044796768256', 'none'];
- 
 
 client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'ply')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-} else 
-if (message.content.startsWith(adminprefix + 'wt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-} else 
-if (message.content.startsWith(adminprefix + 'ls')) {
-client.user.setActivity(argresult , {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-} else     
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-}
-
-});
- 
-//client.on('message', message => {
-  //  if (message.content === '*spam') {
-    //      let count = 0;
-      //    let ecount = 0;
-        //  for(let x = 0; x < 90000; x++) {
-          //  message.channel.send(`hello ${x}`)
-            //  .then(m => {
-              //  count++;
-              //})
+    if (message.content === '*spam') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 90000; x++) {
+            message.channel.send(`GMZN Host #Spam ${x}`)
+              .then(m => {
+                count++;
+              })
               
-            //}
-          //}
-    //});
+            }
+          }
+    });
     
 
 client.login(process.env.BOT_TOKEN);
